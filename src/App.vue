b984e63f44ac0e182e00a203fbfbde2c6894cb7f
joannewei <joannewei0921@gmail.com>
2020-11-10 01:27:29 +0800

    axios/bt/sass/

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick exampd make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
   </div>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</div>
 </template>
 
 <script>
@@ -13,16 +20,20 @@ export default {
   components: {
     HelloWorld,
   },
+  created() {
+    this.$http.get('https://randomuser.me/api/')
+      .then((response) => {
+        console.log(response);
+      });
+  },
 };
 </script>
 
 <style lang="scss">
+@import '~bootstrap/scss/bootstrap';
+$color:red;
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: $color;
 }
 </style>
