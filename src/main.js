c47df017e041e5f34477262d036fae32761d8d97
joannewei <joannewei0921@gmail.com>
2020-11-12 00:15:57 +0800

    在 new Vue 裡面加入 router

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import Vue from 'vue';
 import VueAxios from 'vue-axios';
 import axios from 'axios';
 import App from './App.vue';
-import router from './router/index.js';
+import router from './router';
 
 Vue.use(VueAxios, axios);
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: (h) => h(App),
 }).$mount('#app');
